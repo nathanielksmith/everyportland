27d90b0d8b43e5dd6a5ac602e8a6f90a9aa86e03
Terian Koscik <terian@kongregate.com>
2015-10-03 15:34:13 -0700

    bespoke initial commit

---
+++ bot.py
@@ -0,0 +1,39 @@
+import tweepy # for tweeting
+import secrets # shhhh
+import random
+import inflect
+
+def random_modifier():
+  # open text file
+  text_file = open('modifiers.txt', 'r')
+  text_string = text_file.read()
+  return random.choice(text_string.split('\n'))
+
+def get_next_chunk():
+  # open text file
+  text_file = open('book.txt', 'r+')
+  text_string = text_file.read()
+  chunk = text_string.split('\n')[0]
+
+  # delete what we just tweeted from the text file
+  text_file.seek(0)
+  text_file.write(text_string[len(chunk) + 1:len(text_string)])
+  text_file.truncate()
+  text_file.close()
+
+  p = inflect.engine()
+  return p.plural(chunk)
+
+def portland_word():
+  return random_modifier() + ' ' + get_next_chunk()
+
+def tweet(message):
+  auth = tweepy.OAuthHandler(secrets.consumer_key, secrets.consumer_secret)
+  auth.set_access_token(secrets.access_token, secrets.access_token_secret)
+  api = tweepy.API(auth)
+  auth.secure = True
+  print("Posting message {}".format(message))
+  api.update_status(status=message)
+
+print(portland_word())
+# tweet(portland_word())
